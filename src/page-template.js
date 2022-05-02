b9bd8e21d884acf04ac5ce6442220bf6c304a583
A-Wonderfull-Galaxy <marlonq16@gmail.com>
2022-05-02 18:10:10 -0400

    getting promises and catch

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,5 @@
+const { fstat } = require("fs");
+
 //create the about sectino
 const generateAbout = aboutText => {
   if(!aboutText){
@@ -10,6 +12,25 @@ const generateAbout = aboutText => {
       <p>${aboutText}</p>
     </section>  
   `;
+};
+
+const writeFile = fileContent => {
+  return new Promise((resolve,reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      //if theres an error, reject the promise and send the error to the promises `.catch()` method
+      if(err) {
+        reject(err);
+        //return out of the function here to make sure the Promise doesnt accidentally execute the resolve() function as well
+        return;
+      }
+
+      //if everything went well, resolve the promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    }); 
+  });
 };
 
 //create the project section
@@ -100,3 +121,5 @@ module.exports = templateData => {
   </html>
   `;
 };
+
+module.exports = {writeFile, copyFile} = require('./utils/generate-site.js')
